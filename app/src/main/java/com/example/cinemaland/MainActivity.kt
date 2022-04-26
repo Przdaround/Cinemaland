fe55dabcda3a1a3c008c88928faaa37dbe325edb
prazd <prazdnichnaya@gmail.com>
2022-04-26 18:50:48 +0200

    commit 1

---
+++ app/src/main/java/com/example/cinemaland/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.cinemaland
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+    }
+}
