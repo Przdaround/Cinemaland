7e8d02a735c2cbb07e6c01b320214362a344843e
prazd <prazdnichnaya@gmail.com>
2022-05-23 01:17:21 +0200

    commit 23.5

--- app/src/main/java/com/example/cinemaland/MovieAdapter.kt
+++ app/src/main/java/com/example/cinemaland/MovieAdapter.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
+private const val TAG = "NewsItemAdapter"
+private const val HEADER_VIEW_TYPE = 0
+private const val ITEM_VIEW_TYPE = 1
 
 class MovieAdapter: RecyclerView.Adapter<MovieAdapter.MovieViewHolder2>() {
    private val movielist = ArrayList<Movie>()
@@ -18,21 +20,31 @@ class MovieAdapter: RecyclerView.Adapter<MovieAdapter.MovieViewHolder2>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder2 {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_news, parent, false)
-        return MovieViewHolder2(view)
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            HEADER_VIEW_TYPE -> HeaderMovieViewHolder(
+                inflater.inflate(R.layout.headermovie, parent, false)
+            )
+            else -> MovieViewHolder2(inflater.inflate(R.layout.movie_news))
+        }
     }
+
+
 
     override fun onBindViewHolder(holder: MovieViewHolder2, position: Int) {
         holder.bind(movielist[position])
     }
 
     override fun getItemCount(): Int {
-        return movielist.size
+        return movielist.size + 1
     }
 
     fun addMovie(movie: Movie){
         movielist.add(movie)
         notifyDataSetChanged()
+    }
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) HEADER_VIEW_TYPE else ITEM_VIEW_TYPE
     }
     interface MovieClickListener {
         fun onNewsClick(newsItem: Movie, position: Int)
